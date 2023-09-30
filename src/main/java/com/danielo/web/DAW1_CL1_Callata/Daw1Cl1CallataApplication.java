6c058a570b4bf333cf01b274dcce541356027edf
Danialbert888 <danielo.callata@gmail.com>
2023-09-30 01:43:05 -0500

    CL1 primer commit

---
+++ src/main/java/com/danielo/web/DAW1_CL1_Callata/Daw1Cl1CallataApplication.java
@@ -0,0 +1,13 @@
+package com.danielo.web.DAW1_CL1_Callata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw1Cl1CallataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw1Cl1CallataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielo/web/DAW1_CL1_Callata/Daw1Cl1CallataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielo.web.DAW1_CL1_Callata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw1Cl1CallataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
